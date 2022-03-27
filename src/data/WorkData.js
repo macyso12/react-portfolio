86b08bd2c5a86d856a0701e4d5a583169e0895e0
macyso12 <macyso12@gmail.com>
2022-03-27 11:25:13 -0400

    added work data

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,6 +1,18 @@
 export const Work = [
   {
     id: 1,
+    name: 'Catalyst 2022',
+    description:
+      'Educational web application for new dog-adoptees and shelter organizations',
+    tags: ['Design', 'Figma', 'Designathon'],
+
+    demo:
+      'https://www.figma.com/file/lFKQmwDZB2KzlW5tQcs2Sl/Catalyst-2022?node-id=2%3A2',
+    github:
+      'https://docs.google.com/document/d/1qcMAnl3N2bo9iet1gN_UwsAWeXjf-ih9VP187Zjtm3c/edit?usp=sharing',
+  },
+  {
+    id: 2,
     name: 'Hack4Impact - JDT Lead',
     description:
       'Creating small "lecture-style" projects to demonstrate programming languages from FrontEnd to BackEnd',
@@ -9,7 +21,7 @@ export const Work = [
     github: 'https://github.com/macyso12/Hack4Impact',
   },
   {
-    id: 2,
+    id: 3,
     name: 'BU Student Link Redesign',
     description:
       'Redesign the BU internal student link website from scratch using React.js as the main framework',
@@ -19,7 +31,7 @@ export const Work = [
     github: 'https://github.com/macyso12/Bu-web-recreate',
   },
   {
-    id: 3,
+    id: 4,
     name: 'Cloister Android Application',
     description:
       'Implementing a interactive GUI floor-plan and podcast feature into an Android application from scratch',
