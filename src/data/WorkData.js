e2f0d3c9c009b946d58d910caaebe5026eecf796
macyso12 <macyso12@gmail.com>
2022-07-17 09:38:39 -0700

    updated workdata

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,85 +1,82 @@
 export const Work = [
   {
     id: 1,
-    name: 'Shuttle',
-    description:
-      'A web application where you can search and look for campus clubs [work in progress]',
-    tags: ['Design', 'Figma', 'Campus-Clubs'],
+    name: "BYTE Magazine",
+    description: "A magazine organization that focused in Women in STEM",
+    tags: ["Design", "Figma", "WomeninSTEM"],
 
-    demo:
-      'https://www.figma.com/file/7KeVaB39ZkHoWCLppnvwTU/Shuttle-Draft?node-id=0%3A1',
-    github: '#',
+    demo: "https://www.figma.com/file/7KeVaB39ZkHoWCLppnvwTU/Shuttle-Draft?node-id=0%3A1",
+    github: "#",
     figma:
-      'https://www.figma.com/file/7KeVaB39ZkHoWCLppnvwTU/Shuttle-Draft?node-id=0%3A1',
-  },
-  {
-    id: 1,
-    name: 'Website Templates',
-    description:
-      'A website templates for artist who are looking for minimalistic website look and field [work in progress]',
-    tags: ['Design', 'React', 'Template'],
-
-    demo: 'https://github.com/macyso12/website-template-2.0',
-    github: 'https://github.com/macyso12/website-template-2.0',
-    figma: '#',
-  },
-  {
-    id: 1,
-    name: 'Jingles',
-    description:
-      'An mobile application that recommend trending music and connect you with your friends',
-    tags: ['Design', 'Figma', 'Music'],
-
-    demo:
-      'https://docs.google.com/document/d/1RinN8C3_UqzAA9L-aej44iwUvaMUxyF8H13gGqI0lbk/edit?usp=sharing',
-    github: '#',
-    figma:
-      'https://www.figma.com/file/1OzOEgKfyQDetsdUB0wSE4/Jingle?node-id=3%3A39',
+      "https://www.figma.com/file/G7BI6jRdeALfHmxxg0IZwb/Byte-Magazine?node-id=0%3A1",
   },
   {
     id: 2,
-    name: 'Catalyst 2022',
+    name: "Shuttle",
     description:
-      'Educational web application for new dog-adoptees and shelter organizations',
-    tags: ['Design', 'Figma', 'Designathon'],
+      "A web application where you can search and look for campus clubs [work in progress]",
+    tags: ["Design", "Figma", "Campus-Clubs"],
 
-    demo: 'https://devpost.com/software/dustypaws',
-    github: '#',
+    demo: "https://www.figma.com/file/7KeVaB39ZkHoWCLppnvwTU/Shuttle-Draft?node-id=0%3A1",
+    github: "https://github.com/macyso12/shuttle",
     figma:
-      'https://www.figma.com/file/lFKQmwDZB2KzlW5tQcs2Sl/Catalyst-2022?node-id=2%3A2',
+      "https://www.figma.com/file/7KeVaB39ZkHoWCLppnvwTU/Shuttle-Draft?node-id=0%3A1",
   },
   {
     id: 3,
-    name: 'Hack4Impact - JDT Lead',
+    name: "Website Templates",
     description:
-      'Creating small "lecture-style" projects to demonstrate programming languages from FrontEnd to BackEnd',
-    tags: ['Hack4Impact', 'FrontEnd', 'BackEnd'],
+      "A website templates for artist who are looking for minimalistic website look and field [work in progress]",
+    tags: ["Design", "React", "Template"],
 
-    demo: '#',
-    figma: '#',
-    github: 'https://github.com/macyso12/Hack4Impact',
+    demo: "https://github.com/macyso12/website-template-2.0",
+    github: "https://github.com/macyso12/website-template-2.0",
+    figma: "#",
+  },
+  {
+    id: 1,
+    name: "Jingles",
+    description:
+      "An mobile application that recommend trending music and connect you with your friends",
+    tags: ["Design", "Figma", "Music"],
+
+    demo: "https://docs.google.com/document/d/1RinN8C3_UqzAA9L-aej44iwUvaMUxyF8H13gGqI0lbk/edit?usp=sharing",
+    github: "#",
+    figma:
+      "https://www.figma.com/file/1OzOEgKfyQDetsdUB0wSE4/Jingle?node-id=3%3A39",
   },
   {
     id: 4,
-    name: 'BU Student Link Redesign',
+    name: "Catalyst 2022",
     description:
-      'Redesign the BU internal student link website from scratch using React.js as the main framework',
-    tags: ['react', 'styledComponents', 'Figma'],
+      "Educational web application for new dog-adoptees and shelter organizations",
+    tags: ["Design", "Figma", "Designathon"],
 
-    demo: 'https://lnkd.in/eQz5K4Zx',
-    github: 'https://github.com/macyso12/Bu-web-recreate',
-    figma: 'https://lnkd.in/eQz5K4Zx',
+    demo: "https://devpost.com/software/dustypaws",
+    github: "#",
+    figma:
+      "https://www.figma.com/file/lFKQmwDZB2KzlW5tQcs2Sl/Catalyst-2022?node-id=2%3A2",
   },
   {
     id: 5,
-    name: 'Cloister Android Application',
+    name: "Hack4Impact - JDT Lead",
     description:
-      'Implementing a interactive GUI floor-plan and podcast feature into an Android application from scratch',
-    tags: ['Java', 'Android Application'],
+      'Creating small "lecture-style" projects to demonstrate programming languages from FrontEnd to BackEnd',
+    tags: ["Hack4Impact", "FrontEnd", "BackEnd"],
 
-    demo:
-      'https://www.linkedin.com/in/macyso/details/experience/1787584408/multiple-media-viewer/?treasuryMediaId=1635460859291',
-    github: 'https://github.com/macyso12/Cloisters_App',
-    figma: '#',
+    demo: "#",
+    figma: "#",
+    github: "https://github.com/macyso12/Hack4Impact",
+  },
+  {
+    id: 6,
+    name: "BU Student Link Redesign",
+    description:
+      "Redesign the BU internal student link website from scratch using React.js as the main framework",
+    tags: ["react", "styledComponents", "Figma"],
+
+    demo: "https://lnkd.in/eQz5K4Zx",
+    github: "https://github.com/macyso12/Bu-web-recreate",
+    figma: "https://lnkd.in/eQz5K4Zx",
   },
 ];
